f700e3230158fce93f84e9d8a04f16168165d551
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-11-29 21:14:48 -0800

    Cleanup before branching

---
+++ Swifty Chess/Swifty Chess/ChessView.swift
@@ -0,0 +1,19 @@
+//
+//  ChessView.swift
+//  Swifty Chess
+//
+//  Created by Mikael Mukhsikaroyan on 11/27/16.
+//  Copyright © 2016 MSquaredmm. All rights reserved.
+//
+
+import UIKit
+
+class ChessView: UIView {
+
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
